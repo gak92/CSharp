6e593009001cad9a562e8ed9597928aa1e805381
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-25 18:50:31 +0500

    Update AssembliesAndNamespace

--- Practice/AssembliesAndNamespaces/Program.cs
+++ Practice/AssembliesAndNamespaces/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace AssembliesAndNamespaces
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamespaces
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "Write this text into the file";
+            File.WriteAllText(@"./file.txt", text);
+
+            WebClient client = new WebClient();
+            string address = "https://msdn.microsoft.com";
+            string reply = client.DownloadString(address);
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"./msdn.txt", reply);
+
         }
     }
 }
